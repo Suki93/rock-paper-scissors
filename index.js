dcd8d748af70159a757b453dfc62d870ae411fb5
Nemanja <nemanjasurudzic93@gmail.com>
2021-05-29 23:23:10 +0200

    just roll

--- index.js
+++ index.js
@@ -1,46 +1,17 @@
+let playerScore = 0;
+let computerScore = 0;
+let gamesPlayed = 0;
 
-const playerSelection = "rock";
+document.addEventListener("click", gameSelection);
 
-
-let player = 0;
-let computer = 0;
-let gamesPlayed = 0; 
-
-function computerPlay() {
-    arr = ["rock", "paper", "scissors"];
-    let random = Math.floor(Math.random() * arr.length);
-    return arr[random];
-}
-
-function playRound(playerSelection) {
-    let computerSelection = computerPlay();
-    if (playerSelection == computerSelection) {
-        return "TIE";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        player++;
-        return "Player wins!";
-    } else if (computerSelection == "rock" && playerSelection == "scissors") {
-        computer++;
-        return "Computer wins!";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        computer++;
-        return "Computer wins!";
-    } else if (computerSelection == "rock" && playerSelection == "paper") {
-        player++;
-        return "Player wins!"; 
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        player++;
-        return "Player wins!";
-    } else if (computerSelection == "scissors" && playerSelection == "paper") {
-        player++;
-        return "Computer wins!";
-    } else {
-        return "Put in scissors, rock or paper";
+function gameSelection(event) {
+    let element = event.target;
+    console.log(event.target)
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+    if (element.classList.contains("picture") && element.id === ("rock")) {
+        playRound(rock);
+        console.log("submitted rock");
     }
 }
-
-
-
-
-
-
